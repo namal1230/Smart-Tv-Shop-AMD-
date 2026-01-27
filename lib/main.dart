c32190fd69331184798352d3a0cae482ccdd9363
Namal Dilmith Ruwanpathirana <prowaddosinhalatutorial@gmail.com>
2026-01-27 21:43:36 +0530

    firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:smart_tv_shop/screens/auth/login_screen.dart';
 import 'package:smart_tv_shop/screens/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
